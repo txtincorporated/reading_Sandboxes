b0bac3a56d886864b70dafbc29112414453ca84b
Nathan P. Keene <npk@txtincorporated.com>
2016-06-14 12:57:50 -0700

    class07_prep05

--- class07_prep/script.js
+++ class07_prep/script.js
@@ -1,5 +1,5 @@
-      var colors = [tammy = {name : 'Tammy', color : 'blue'}, joe = {name : 'Joe', color : 'green'}, tamika = {name : 'Tamika', color : 'red'}, tom = {name : 'Tom', color : 'yellow'}];
-
-      var who = colors.filter(function(ele) {return ele.name.length > 3;});
+      var colors = [tammy = {name : 'Tammy', color : 'blue', height : 173}, joe = {name : 'Joe', color : 'green', height : 177}, tamika = {name : 'Tamika', color : 'red', height : 162}, tom = {name : 'Tom', color : 'yellow', height : 170}];
+      console.log('Hi');
+      var who = colors.sort(function(a,b) {return a.height-b.height;});
 
       console.log(who);
