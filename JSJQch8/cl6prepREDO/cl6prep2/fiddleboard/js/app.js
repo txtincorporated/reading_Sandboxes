faeede1194caa52c08577dbb333eb44c33785618
Nathan P. Keene <npk@txtincorporated.com>
2016-06-17 07:17:48 -0700

    cl6prepREDO

---
+++ JSJQch8/cl6prepREDO/cl6prep2/fiddleboard/js/app.js
@@ -0,0 +1,44 @@
+//var strings = [];
+var x = {
+  bigness:'large',
+  boldness: 'max',
+
+  annamethod4U: function() {
+    // var xString = JSON.stringify(this);
+    // strings.push(xString);
+    var message = 'Good Luck, Max!';
+    return message;
+  }
+};
+console.log(x.annamethod4U());
+$('header#bonVoyage').html('<span>' + x.annamethod4U() + '</span>');
+
+//Special thx to https://gist.github.com/cowboy/3749767 for this cheesey stunt
+// var stringify = function(obj, prop) {
+//   var placeholder = '____PLACEHOLDER____';
+//   var fns = [];
+//   var json = JSON.stringify(obj, function(key, value) {
+//     if (typeof value === 'function') {
+//       fns.push(value);
+//       return placeholder;
+//     }
+//     return value;
+//   }, 2);
+//   json = json.replace(new RegExp('"' + placeholder + '"', 'g'), function(_) {
+//     return fns.shift();
+//   });
+//   return 'this ["' + prop + '"] = ' + json + ';';
+// };
+//
+// strings.push(stringify(x, 'x'));
+
+function replaceX(key, value) {
+  if (key === 'bigness') {
+    return 'super';
+  }
+  return value;
+};
+
+var newString = JSON.stringify(x, replaceX);
+var nextString = newString.replace(/boldness/, 'badness');
+var y = JSON.parse(nextString);
