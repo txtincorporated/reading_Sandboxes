5b85b450725106e605d2506fe6f010dc1ddc0792
Nathan P. Keene <npk@txtincorporated.com>
2016-07-04 13:28:00 -0700

    cl07prep0101

--- class07_prep/script.js
+++
@@ -1,5 +0,0 @@
-      var colors = ['{"name" : "Tammy", "color" : "blue"}', '{"name" : "Joe", "color" : "green"}', '{"name" : "Tamika", "color" : "red"}', '{"name" : "Tom", "color" : "yellow"}'];
-
-      colors.shift();
-
-      console.log(colors);
