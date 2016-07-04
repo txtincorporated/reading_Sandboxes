42df4f20c2a3b0d75a6b90bfa0a88ed875e88ba4
Nathan P. Keene <npk@txtincorporated.com>
2016-07-04 13:57:37 -0700

    file cleanup

--- class07_prep/script.js
+++
@@ -1,5 +0,0 @@
-      var colors = [tammy = {name : 'Tammy', color : 'blue', height : 173}, joe = {name : 'Joe', color : 'green', height : 177}, tamika = {name : 'Tamika', color : 'red', height : 162}, tom = {name : 'Tom', color : 'yellow', height : 170}];
-      console.log('Hi');
-      var who = colors.sort(function(a,b) {return a.height-b.height;});
-
-      console.log(who);
