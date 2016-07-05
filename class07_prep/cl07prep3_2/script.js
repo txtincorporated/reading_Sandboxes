5cca7871923b7a0c995e1147be4a595047c96e7f
Nathan P. Keene <npk@txtincorporated.com>
2016-07-04 20:46:06 -0700

    cl07prep3_2 commit 1

--- class07_prep/cl07prep3_2/script.js
+++ class07_prep/cl07prep3_2/script.js
@@ -1,11 +1,11 @@
       var capitals = [{country : 'France', capital : 'Paris'}, {country : 'Germany', capital : 'Berlin'}, {country : 'England', capital : 'London'}, {country : 'Spain', capital : 'Madrid'}];
 
-      function G(item, prompt) {
+      function G(item) {
         var results = document.getElementById('results');
-        results.innerHTML = results.innerHTML + 'The ' + prompt[0] + ' of ' + item.country + ' is ' + item.capital + '.<br>';
+        results.innerHTML = results.innerHTML + 'The ' + item.property[0] + ' of ' + item.country + ' is ' + item.capital + '.<br>';
       }
 
       var who = function(array) {
-        return array.forEach(G);
+        return array.map(G);
       };
       who(capitals, 'capital');
