49c95e8b703d12952241ef11f6eafebaad78a598
Nathan P. Keene <npk@txtincorporated.com>
2016-07-04 21:05:52 -0700

    Working version of cl07prep3_2 still needs HOF-ication

--- class07_prep/cl07prep3_2/script.js
+++ class07_prep/cl07prep3_2/script.js
@@ -1,11 +1,14 @@
-      var capitals = [{country : 'France', capital : 'Paris'}, {country : 'Germany', capital : 'Berlin'}, {country : 'England', capital : 'London'}, {country : 'Spain', capital : 'Madrid'}];
+      var capitals = [{owner : 'France', predicate : 'Paris', significance : 'capital city'}, {owner : 'Germany', predicate : 'Berlin', significance : 'capital city'}, {owner : 'England', predicate : 'London', significance : 'capital city'}, {owner : 'Spain', predicate : 'Madrid', significance : 'capital city'}];
+
+      var examples = [{owner : '9', predicate : '3', significance : 'square root'}, {owner : '2 and 5', predicate : '10', significance : 'product'}, {owner : 'Spain', predicate : 'Felipe IV', significance : 'king'}, {owner : 'being earnest', predicate : 'not to be underestimated', significance : 'importance'}];
 
       function G(item) {
         var results = document.getElementById('results');
-        results.innerHTML = results.innerHTML + 'The ' + item.property[0] + ' of ' + item.country + ' is ' + item.capital + '.<br>';
+        results.innerHTML = results.innerHTML + 'The ' + item.significance + ' of ' + item.owner + ' is ' + item.predicate + '.<br>';
       }
 
       var who = function(array) {
         return array.map(G);
       };
-      who(capitals, 'capital');
+      who(capitals);
+      who(examples);
